1d520bb063a49fd2552364ec971547c120edf059
EmyJuly <farsimane00@gmail.com>
2024-05-01 20:55:55 +0000

    all

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,37 +1,36 @@
 #!/usr/bin/env python3
-""" Implementing an expiring web cache and tracker """
+"""
+web cache and tracker
+"""
+import requests
 import redis
 from functools import wraps
-import requests
-from typing import Callable
+
+store = redis.Redis()
 
 
-def tracker(fn: Callable) -> Callable:
-    """Decorator for get_page"""
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cache_page = client.get(f'{url}')
-        if cache_page:
-            return cache_page.decode('utf-8')
-        try:
-            request = fn(url)
-        except Exception as e:
-            return f"Error: {e}"
-        client.set(f'{url}', request, ex=10)
-        return request
+        count_key = "count:" + url
+        html = method(url)
 
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
 
-@tracker
+@count_url_access
 def get_page(url: str) -> str:
-    """
-    Obtain the HTML content of a particular URL and return it.
-
-    :param url: The URL of the page.
-    """
-    req = requests.get(url)
-    return req.text
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
